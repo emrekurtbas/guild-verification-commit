29d391ac3fed2811b04044a17e7d0eb51d416dfa
Emre KURTBAŞ <39852076+emrekurtbas@users.noreply.github.com>
2025-10-30 23:51:46 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T20:51:44.247Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-10-30T20:51:45.876Z
+Commit: Fix CSS styling bug on mobile devices
